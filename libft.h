7d1c54f65873f74fa4cf11c76e1d466eb0bd1737
omaalii <omarmoustafa0024@gmail.com>
2023-10-25 16:31:21 +0200

    real pre finale

--- libft.h
+++ libft.h
@@ -18,12 +18,13 @@
 #include <unistd.h>
 #include "libft.h"
 int		ft_isalnum(int c);
-size_t		ft_strlen(const char *str);
+size_t	ft_strlen(const char *str);
 int		ft_isprint(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isalpha(int c);
 void	ft_bzero(void *str, size_t n);
+void    *ft_calloc(size_t count, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
 int		ft_toupper(int c);
@@ -38,6 +39,16 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		*ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
-// void	*ft_calloc(size_t count, size_t size);
-// char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+static int	ft_len_int(int nb);
+char    ft_strdup(const char *s1);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+char	*ft_strtrim(char const *s1, char const *set);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int nb, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 #endif
